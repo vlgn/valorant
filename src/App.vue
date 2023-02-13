b913086db828fa2c5e78b1b50be623f68b040062
scdlr <scdlr.dev@gmail.com>
2023-02-13 06:56:12 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,145 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="search">
+      <div class="search__input">
+        <input type="text" placeholder="Playername#Tag" class="search__input" />
+        <i class="uil uil-search"></i>
+      </div>
+      <region-dropdown :regions="regions"></region-dropdown>
+    </div>
+    <div class="filter">
+      <div class="filter__icon">
+        <img src="./assets/radiant-badge.png" alt="rank-icon" />
+      </div>
+      <div class="filter__rank">
+        <h2 class="filter__rank-title">leaderboard</h2>
+        <h3 class="filter__rank-name">radiant</h3>
+      </div>
+      <div class="filter__episode">
+        <h2 class="filter__episode-name">episode 1</h2>
+        <h3 class="filter__act-name">act 1</h3>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import regionDropdown from "./components/region-dropdown";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    regionDropdown,
+  },
+  data() {
+    return {
+      regions: [
+        {
+          title: "europe",
+          code: "eu",
+        },
+        {
+          title: "north america",
+          code: "na",
+        },
+        {
+          title: "asia pacific",
+          code: "ap",
+        },
+        {
+          title: "korea",
+          code: "kr",
+        },
+        {
+          title: "brazil",
+          code: "br",
+        },
+        {
+          title: "latin america",
+          code: "latam",
+        },
+      ],
+    };
   },
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+@import "@/scss/_config.scss";
+
+.container {
+  margin: 0 auto;
+  max-width: 120rem;
+}
+.title {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-weight: bold;
+  font-size: 4rem;
+}
+.search {
+  margin-top: 2rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 1rem;
+  &__input {
+    input {
+      padding: 1rem 0.5rem;
+      max-width: 14rem;
+    }
+    input::placeholder {
+      color: grey;
+    }
+    i {
+      cursor: pointer;
+      font-weight: bold;
+    }
+  }
+}
+.filter {
+  color: white;
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  &__icon {
+    background-color: black;
+    width: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  &__rank {
+    padding: 2rem;
+    width: 80%;
+    cursor: pointer;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-transform: uppercase;
+    text-align: center;
+  }
+  &__rank-name {
+    margin-top: 1rem;
+    font-size: 4rem;
+    font-weight: 700;
+  }
+  &__episode {
+    padding: 2rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-transform: uppercase;
+    font-weight: bold;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    width: 15%;
+  }
+  &__act-name {
+    margin-top: 1rem;
+    font-weight: 700;
+    font-size: 3rem;
+  }
 }
 </style>
---
+++ src/components/region-dropdown.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="region-dropdown">
+    <div @click="isDropdownVisible()" class="region-dropdown__selected-region">
+      <span>{{ selectedRegion }}</span>
+      <i class="uil uil-arrow-down"></i>
+    </div>
+    <div :class="{ active: show }" class="region-dropdown__options">
+      <div
+        v-for="(item, index) in regions"
+        :key="index"
+        @click="regionSelect(item.title)"
+        class="region-dropdown__options-item"
+      >
+        {{ item.title }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "regionDropdown",
+  props: {
+    regions: Object,
+    // default() {
+    //   return { title: "europe" };
+    // },
+  },
+  data() {
+    return {
+      selectedRegion: "europe",
+      show: false,
+    };
+  },
+  methods: {
+    regionSelect(region) {
+      this.selectedRegion = region;
+      this.show = !this.show;
+    },
+    isDropdownVisible() {
+      this.show = !this.show;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.region-dropdown {
+  cursor: pointer;
+  background-color: green;
+  position: relative;
+  width: 12rem;
+  &__selected-region {
+    padding: 0 1rem;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    box-shadow: 0 5px 20px rgba(0, 0, 0, 0.05);
+  }
+  &__options {
+    display: none;
+    position: absolute;
+    top: 2rem;
+    width: 100%;
+    background-color: #fff;
+    box-shadow: 0 30px 30px rgba(0, 0, 0, 0.05);
+    overflow: hidden;
+    &-item {
+      padding: 0 1rem;
+      cursor: pointer;
+      &:hover {
+        color: #fff;
+        background: darkblue;
+      }
+    }
+  }
+  .active {
+    display: block;
+    .region-dropdown__selected-region i {
+      transform: rotate(-180deg);
+    }
+  }
+}
+</style>
